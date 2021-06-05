a6f8a3991abd16dfe9d57a48aaf2a8b8033f3f31
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2021-06-05 14:23:53 +0300

    попытки в базу

--- FlowModelMobileApp/FlowModelMobileApp/App.xaml.cs
+++ FlowModelMobileApp/FlowModelMobileApp/App.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace FlowModelMobileApp
 {
    public partial class App : Application
-   {
+   { 
       public App()
       {
          InitializeComponent();
@@ -22,6 +24,39 @@ namespace FlowModelMobileApp
 
       protected override void OnResume()
       {
+      }
+
+      //https://metanit.com/sharp/xamarin/7.3.php гайд про базу
+      //https://itvdn.com/ru/channel/video/it-mobile-dev-android-sqlite-grouping-sorting-filtering
+      public const string DATABASE_NAME = "users.db";
+      public static UserAsyncRepository database;
+      public static UserAsyncRepository Database
+      {
+         get
+         {
+            if (database == null)
+            {
+               // путь, по которому будет находиться база данных
+               string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME);
+               // если база данных не существует (еще не скопирована)
+               if (!File.Exists(dbPath))
+               {
+                  // получаем текущую сборку
+                  var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+                  // берем из нее ресурс базы данных и создаем из него поток
+                  using (Stream stream = assembly.GetManifestResourceStream($"HelloApp.{DATABASE_NAME}"))
+                  {
+                     using (FileStream fs = new FileStream(dbPath, FileMode.OpenOrCreate))
+                     {
+                        stream.CopyTo(fs);  // копируем файл базы данных в нужное нам место
+                        fs.Flush();
+                     }
+                  }
+               }
+               database = new UserAsyncRepository(dbPath);
+            }
+            return database;
+         }
       }
    }
 }
---
+++ FlowModelMobileApp/FlowModelMobileApp/UserAsyncRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+
+namespace FlowModelMobileApp
+{
+   public class UserAsyncRepository
+   {
+      SQLiteAsyncConnection database;
+
+      public UserAsyncRepository(string databasePath)
+      {
+         database = new SQLiteAsyncConnection(databasePath);
+      }
+
+      public async Task CreateTable()
+      {
+         await database.CreateTableAsync<Users>();
+      }
+      public async Task<List<Users>> GetItemsAsync()
+      {
+         return await database.Table<Users>().ToListAsync();
+
+      }
+      public async Task<Users> GetItemAsync(int id)
+      {
+         return await database.GetAsync<Users>(id);
+      }
+      public async Task<int> DeleteItemAsync(Users item)
+      {
+         return await database.DeleteAsync(item);
+      }
+      public async Task<int> SaveItemAsync(Users item)
+      {
+         if (item.UserId != 0)
+         {
+            await database.UpdateAsync(item);
+            return item.UserId;
+         }
+         else
+         {
+            return await database.InsertAsync(item);
+         }
+      }
+   }
+}
