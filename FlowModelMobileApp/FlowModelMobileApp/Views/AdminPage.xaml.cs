1177b99226842e0ae4625f382639c6f137d6b185
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2021-06-11 15:53:35 +0300

    nothing new

--- FlowModelMobileApp/FlowModelMobileApp/Views/AdminPage.xaml.cs
+++ FlowModelMobileApp/FlowModelMobileApp/Views/AdminPage.xaml.cs
@@ -16,37 +16,13 @@ namespace FlowModelMobileApp.Views
       public AdminPage()
       {
          InitializeComponent();
-         RolePicker.Items.Insert(0, "Администратор");
-         RolePicker.Items.Insert(1, "Исследователь");
       }
 
-      private void RolePicker_OnSelectedIndexChanged_SelectedIndexChanged(object sender, EventArgs e)
-      {
-         if (RolePicker.SelectedIndex == 0)
-         {
-            selectedRole = "admin";
-         }
-         else
-         {
-            selectedRole = "research";
-         }
-      }
+     
 
-      async private void UserAdd(object sender, EventArgs e)
+      private void AddUserButtonClick(object sender, EventArgs e)
       {
-         Users users = new Users()
-         {
-            Login = LoginText.Text,
-            Password = PasswordText.Text,
-            Role = selectedRole
-         };
-
-         using (SQLiteConnection conn = new SQLiteConnection(App.usersFilePath))
-         {
-            conn.CreateTable<Users>();
-            int rowsAdded = conn.Insert(users);
-            await DisplayAlert("Успех", "Пользователь добавлен успешно!","OK");
-         }
+         Navigation.PushAsync(new AddUser());
       }
    }
 }
